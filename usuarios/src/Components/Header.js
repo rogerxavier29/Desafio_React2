8a76542d3e1fc5d7261982df50b032852bb9187e
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-01-27 18:12:01 -0300

    Header-Login
    passado no Header.js link de login e Home

--- usuarios/src/Components/Header.js
+++ usuarios/src/Components/Header.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
