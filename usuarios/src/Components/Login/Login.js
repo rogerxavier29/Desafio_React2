9ba97ed86a36fc6bc72b200eb9229b9d57f088d0
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-01-28 18:38:25 -0300

    login.js
    criado login.js
    criados rotas com BrowserRouter, Routes, Route
    e passado em Route LoginForm que não esta pegando
    e LoginCreate

--- usuarios/src/Components/Login/Login.js
+++ usuarios/src/Components/Login/Login.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import LoginCreate from './LoginCreate/LoginCreate';
+import LoginForm from './LoginForm/LoginForm';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="./LoginForm/LoginForm" element={<LoginForm />} />
+          <Route path="/LoginCreate/LoginCreate" element={<LoginCreate />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default Login;
