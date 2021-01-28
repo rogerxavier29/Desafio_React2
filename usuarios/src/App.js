4d721130bdcfbea91e38f94724ea5b289abe1fb3
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-01-28 18:29:02 -0300

    App.js
    passado outro caminho no route login

--- usuarios/src/App.js
+++ usuarios/src/App.js
@@ -15,7 +15,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="./Components/Login/Login" element={<Login />} />
         </Routes>
         <Footer />
       </BrowserRouter>
