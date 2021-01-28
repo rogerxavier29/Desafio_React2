f7c7aaea37fcf26bc3bf4b983e8f2e5730d64b1f
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-01-28 15:33:21 -0300

    Footer.js e styles.js do footer
    passado Estutruraf =footer  e um p,
    
    styles.js Estruturaf =footer
    colocado width 100%, altura de 64px, background e border top

---
+++ usuarios/src/Components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Estruturaf } from './styles';
+
+const Footer = () => {
+  return (
+    <Estruturaf>
+      <p>Relíquias. Alguns direitos Reservados</p>
+    </Estruturaf>
+  );
+};
+
+export default Footer;
---
+++ usuarios/src/Components/Footer/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Estruturaf = styled.footer`
+  width: 100%;
+  height: 64px;
+  background: #4169e1;
+  border-top: 3px solid black;
+`;
