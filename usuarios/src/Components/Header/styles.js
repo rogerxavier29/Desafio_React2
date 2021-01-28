68284773f34582ad99e89fe4fe75cf2c0ce24ea6
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-01-28 15:30:48 -0300

    styles.js do header.js
    retirado o height da estrutura e colocado no container, passado top 0px e border-bottom: tamanho  o tipo e a cor. depois em container substituido margin-top de 20 pra 10px e acrescentado o height da estrutura

--- usuarios/src/Components/Header/styles.js
+++ usuarios/src/Components/Header/styles.js
@@ -2,18 +2,20 @@ import styled from 'styled-components';
 
 export const Estrutura = styled.header`
   width: 100%;
-  height: 80px;
   background: #4169e1;
   position: fixed;
   z-index: 100;
+  top: 0px;
+  border-bottom: 3px solid black;
 `;
 
 export const Container = styled.nav`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin-top: 20px;
+  margin-top: 10px;
   margin-left: 30px;
+  height: 64px;
   h1 {
     margin-left: 100px;
     color: gold;
