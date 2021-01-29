fe8cc586906d96d91bb16cf30daf17c70e8301ea
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-01-28 22:45:05 -0300

    Login.js
    passando caminho correto Route de LoginForm e LoginCreate

--- usuarios/src/Components/Login/Login.js
+++ usuarios/src/Components/Login/Login.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
-import LoginCreate from './LoginCreate/LoginCreate';
 import LoginForm from './LoginForm/LoginForm';
+import LoginCreate from './LoginCreate/LoginCreate';
 
 const Login = () => {
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="./LoginForm/LoginForm" element={<LoginForm />} />
-          <Route path="/LoginCreate/LoginCreate" element={<LoginCreate />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="./LoginCreate" element={<LoginCreate />} />
+      </Routes>
     </div>
   );
 };
