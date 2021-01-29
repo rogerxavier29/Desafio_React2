80f418f2339d4b42bec86b6dd0c112204651e412
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-01-29 12:44:47 -0300

    LoginForm.js
    em return passado estrutura que ira pegar tudo, dentro dela colocado form passado a funcao handlesubmit no memso, colocado o titulo
    depois criado tres label e tres input , onde foi colocado o tipo de cada de acordo, placeholder, passado value, conforme as const criados no inicio do cod, e chamado a funcao onChange para cada um. Depois criado um botao de entrar e link para pessoa se cadastrar

--- usuarios/src/Components/Login/LoginForm/LoginForm.js
+++ usuarios/src/Components/Login/LoginForm/LoginForm.js
@@ -1,7 +1,47 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Estrutura, Formulario, Titulo } from './styles';
 
 const LoginForm = () => {
-  return <div>Login Form</div>;
+  const [nomeUsuario, setNomeUsuario] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [senha, setSenha] = React.useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <Estrutura>
+      <Formulario action="" onSubmit={handleSubmit}>
+        <Titulo>Login do Usuário</Titulo>
+        <label htmlFor="nomeUsuario">Usuario</label>
+        <input
+          type="text"
+          placeholder="Digite seu Nome"
+          value={nomeUsuario}
+          onChange={({ target }) => setNomeUsuario(target.value)}
+        />
+        <label htmlFor="Email">Email</label>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={({ target }) => setEmail(target.value)}
+        />
+        <label htmlFor="Senha">Senha</label>
+        <input
+          type="password"
+          placeholder="Senha"
+          value={senha}
+          onChange={({ target }) => setSenha(target.value)}
+        />
+        <button>Entrar</button>
+        <Link to="/login/cadastrar">Quero Me Cadastrar</Link>
+      </Formulario>
+    </Estrutura>
+  );
 };
 
 export default LoginForm;
