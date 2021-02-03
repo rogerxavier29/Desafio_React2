aedc87b9783cfc4f1704b39b1127a48c6f51bb78
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-02-03 15:13:50 -0300

    useForm.js
    criado o validar onde foi passado email e password, onde ambos tem seus respectivos regex, que seria a condição para esses campos terem validade.
    
    passado o type em useForm, pois la loginform.js irei chamar email e password.
    
    criado error setError em estado null, primeiramente.  O mesmo sera utilizado para setar error na função para validar os valores ou não.
    
    criada a função validate, passado  valor, que sera validado. 1° if verifica se o tipo ja foi definido o 2° if verifica se o campo foi preenchido, caso não chamo um erro com setError ou caso não ocorra return false
    caso não ocorra sera verificado se possui o tipo do validar, se falso ira chamar o erro de novo. E caso não haja erro o mesmo sera nulo, não havera.
    
    em function OnChange
    passando if error no validate de onChange, para verificar se possui erro.
    
    passado em return error, validate ja como metodo e passando valor value para verificacao, passando tambem onBlur para validar o usuario ao clicar o campo

--- usuarios/src/Hooks/useForm.js
+++ usuarios/src/Hooks/useForm.js
@@ -1,15 +1,46 @@
 import React from 'react';
 
-const useForm = () => {
+const validar = {
+  email: {
+    regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    message: 'Preencha um email valido',
+  },
+  password: {
+    regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+    message:
+      'A senha precisa ter 1 caracter maíusculo, 1 minúsculo e 1 digito. Com no mínimo 8 caracteres.',
+  },
+};
+
+const useForm = (type) => {
   const [value, setValue] = React.useState('');
+  const [error, setError] = React.useState('');
+
+  function validate(value) {
+    if (type === false) return true;
+    if (value.length === 0) {
+      setError('Preencha um valor');
+      return false;
+    } else if (validar[type] && !validar[type].regex.test(value)) {
+      setError(validar[type].message);
+      return false;
+    } else {
+      setError(null);
+      return true;
+    }
+  }
 
   function onChange({ target }) {
+    if (error) validate(target.value);
     setValue(target.value);
   }
   return {
     value,
     setValue,
     onChange,
+    error,
+    validate: () => validate(value),
+    onBlur: () => validate(value),
   };
 };
 
