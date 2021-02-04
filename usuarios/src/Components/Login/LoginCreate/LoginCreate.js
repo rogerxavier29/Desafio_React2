385a2e14d94952be00fd199abd9cc9b359998835
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-02-04 18:45:12 -0300

    LoginCreate.js
    importado axios e useForm.
    
    importando email e senha com useForm, contendo tudo o que contem eles valor, setarvalor onChange.
    
    criado a funcao de handle submit e chamado o mesmo no formulari e dentro dela passado o axios.post passado a url , chamado e referencia do email e referenciando o mesmo com o seu valor o mesmo para senha.
    Nos inputs acrescentado a label, seus type, name, placeholder e desestruturao o objeto

--- usuarios/src/Components/Login/LoginCreate/LoginCreate.js
+++ usuarios/src/Components/Login/LoginCreate/LoginCreate.js
@@ -1,15 +1,50 @@
 import React from 'react';
 import Input from '../../Form/Input';
+import axios from 'axios';
+import useForm from '../../../Hooks/useForm';
 
 import { Estrutura, Formulario, Titulo, Botao } from './styles';
 
 const LoginCreate = () => {
+  const email = useForm('email');
+  const senha = useForm('password');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    axios
+      .post('https://desafionodegx2.herokuapp.com/sessions/', {
+        email: email.value,
+        password: senha.value,
+      })
+      .then(
+        (response) => {
+          console.log(response);
+        },
+        (error) => {
+          console.log(error);
+        },
+      );
+  }
+
   return (
     <Estrutura>
-      <Formulario>
+      <Formulario onSubmit={handleSubmit}>
         <Titulo>Login: </Titulo>
-        <Input />
-        <Input />
+        <Input
+          label="Email"
+          type="email"
+          name="email"
+          placeholder="Digite seu email"
+          {...email}
+        />
+        <Input
+          label="Senha"
+          type="password"
+          name="senha"
+          placeholder="Digite sua senha"
+          {...senha}
+        />
         <Botao>Entrar</Botao>
       </Formulario>
     </Estrutura>
