4e431387b71afee95f6a2069629529ecd2bc92bf
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-02-04 16:00:48 -0300

    styles.js do LoginCreate.js
    importado styled-components.
    
    estrutura: passado background azul, colocado tamanho de 540 para pegar toda a pag, padding-bottom de 200px e padding-top de 30px para não ficar em branco
    
    formulario: borda pontilhada com dashed, background cinza, tamanho e altura 500px e 450px, margin 0 auto pra ficar ao centro, margin-top pra distancia do topo, passando tambem como display grid e grid-template 1fr para ficar um abaixo do outro.
    input: passado tamanho de 250px  altura de 25px, margin-left: 120px, border-radius: 3px
    
    estilzado o titulo tambem, margin-left pra desgrudar da esquerda, altura 0px, margin-top 5px e font-size de 30px
    
    botao passado tamanho de 150px e heigth de 30px borda arredondada 5px, e margin auto para ficar ao centro

---
+++ usuarios/src/Components/Login/LoginCreate/styles.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+export const Estrutura = styled.div`
+  background: #87cefa;
+  height: 540px;
+  padding-bottom: 200px;
+  padding-top: 30px;
+`;
+
+export const Formulario = styled.form`
+  border-style: dashed;
+  background: #d3d3d3;
+  width: 500px;
+  height: 450px;
+  margin: 0 auto;
+  margin-top: 30px;
+  display: grid;
+  grid-template-columns: 1fr;
+  input {
+    width: 250px;
+    height: 25px;
+    margin-left: 120px;
+    border-radius: 3px;
+  }
+`;
+
+export const Titulo = styled.h1`
+  margin-left: 10px;
+  height: 0px;
+  margin-top: 5px;
+  font-size: 30px;
+`;
+
+export const Botao = styled.button`
+  width: 150px;
+  height: 30px;
+  border-radius: 5px;
+  margin: 0 auto;
+`;
