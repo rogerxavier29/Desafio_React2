fd89275a5c31c8d79b7e33f2fbf13b5d836dc5ab
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-02-04 14:12:02 -0300

    LoginForm.js
    passando o caminho correto.

--- usuarios/src/Components/Login/LoginForm/LoginForm.js
+++ usuarios/src/Components/Login/LoginForm/LoginForm.js
@@ -24,7 +24,7 @@ const LoginForm = () => {
         .then(
           (response) => {
             if (response.data === response.data) {
-              window.location.replace('/cadastrar/login');
+              window.location.replace('/Login/logar');
             }
           },
           (error) => {
