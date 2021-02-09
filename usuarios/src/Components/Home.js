f06c6fd07f8b58b9614b071d6800db2317a7fa11
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-02-09 11:45:16 -0300

    Home.js
    importado input.
    styled component
    passado div  como estrutura
    tittle como h1.
    form como formulario e 2 Inputs importado e o 2 dois criados e um button.

--- usuarios/src/Components/Home.js
+++ usuarios/src/Components/Home.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import Input from './Form/Input';
+
+import { Estrutura, Title1, Formulario } from './styles';
 
 const Home = () => {
   return (
-    <div>
+    <Estrutura>
       Home
       <p>Bem-vindo, {sessionStorage.getItem('name')}</p>
-    </div>
+      <Title1>Adicionar Produtos</Title1>
+      <Formulario>
+        <Input label="Nome" />
+        <Input label="Descrição" />
+        <input label="Imagem" />
+        <input label="Logo" />
+        <button>Add Produto</button>
+      </Formulario>
+    </Estrutura>
   );
 };
 
