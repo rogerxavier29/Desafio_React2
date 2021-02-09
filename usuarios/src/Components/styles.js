62d30088596dead8428532536645b715f0e7bbe9
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-02-09 11:51:12 -0300

    styles.js do home.js
    passado a estrutura padrao das paginas  com background azul, altura de 540px, padding bottom de 200px e padding-top  de 30px
    
    title padding-top: 30px para se afastar do topo e margin-left para afastar a esquerda.
    
    formulario:
    passado um margin-top de 10px,
    margin-left:15px,
    tamanho de 900px e altura de 200px, não muito grande, borda pontilhada dashed, display: flex, para ficar um do lado do outro, justify-content: space-between.
    input: margin-left e right para não ficar colado nas pontas, passado witdth e height tamanho e altura regular e borda arrendondada de 3px

---
+++ usuarios/src/Components/styles.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const Estrutura = styled.div`
+  background: #87cefa;
+  height: 540px;
+  padding-bottom: 200px;
+  padding-top: 30px;
+`;
+
+export const Title1 = styled.h1`
+  padding-top: 30px;
+  margin-left: 10px;
+`;
+
+export const Formulario = styled.form`
+  margin-top: 10px;
+  margin-left: 15px;
+  width: 900px;
+  height: 200px;
+  border-style: dashed;
+  display: flex;
+  justify-content: space-between;
+  input {
+    margin-left: 5px;
+    margin-right: 5px;
+    margin-top: 40px;
+    width: 150px;
+    height: 20px;
+    border: 3px;
+  }
+`;
