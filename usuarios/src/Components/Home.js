80c5a94f4195fcbbad2eedef437b0a9592412aac
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-02-09 19:05:22 -0300

    home.js
    importado useForm e axios
    passando o estado de cada input, nomeProduto  e descrição passado como useForm()
    
    logo e manual passado como useState, como objeto vazio
    
    feita a funcao handlesubmit para registra os valores do form,
    
    dentro dela passado axios metodo post, com name, descricao logo  e manual
    
    e por ultimo fiz uma função  handleLogoChange e setar o logo input do logo.
    passado handlesubmit no formulario.
    nos input nome e descrição, chamado label, type , name e desestruturado  cada um dele.
     em logo  e manual passado input normais não importados, sao type file onChange chamando a função feita para podermos setar e manual passado accept application/pdf para so permitir arquivos pdf

--- usuarios/src/Components/Home.js
+++ usuarios/src/Components/Home.js
@@ -1,20 +1,63 @@
 import React from 'react';
+import useForm from '../Hooks/useForm';
 import Input from './Form/Input';
+import axios from 'axios';
 
 import { Estrutura, Title1, Formulario } from './styles';
 
 const Home = () => {
+  const nomeProduto = useForm();
+  const descricao = useForm();
+  const [logo, setLogo] = React.useState({});
+  const [manual, setManual] = React.useState({});
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    axios
+      .post('https://desafionodegx2.herokuapp.com/products/', {
+        name: nomeProduto.value,
+        descricao: descricao.value,
+        logo,
+        manual,
+      })
+      .then(
+        (response) => {
+          console.log(response);
+        },
+        (error) => {
+          console.log(error);
+        },
+      );
+  }
+  function handleLogoChange({ target }) {
+    setLogo({
+      raw: target.files[0],
+    });
+  }
+
   return (
     <Estrutura>
-      Home
       <p>Bem-vindo, {sessionStorage.getItem('name')}</p>
       <Title1>Adicionar Produtos</Title1>
-      <Formulario>
-        <Input label="Nome" />
-        <Input label="Descrição" />
-        <input label="Imagem" />
-        <input label="Logo" />
-        <button>Add Produto</button>
+      <Formulario onSubmit={handleSubmit}>
+        <Input
+          label="Nome"
+          type="text"
+          name="nomeProduto"
+          placeholder="Nome do Produto"
+          {...nomeProduto}
+        />
+        <Input
+          label="Descrição"
+          type="text"
+          name="descricao"
+          placeholder="Faça a descrição do Produto"
+          {...descricao}
+        />
+        <input label="Imagem" type="file" onChange={handleLogoChange} />
+        <input label="Logo" type="file" accept="application/pdf" />
+        <button type="submit">Add Produto</button>
       </Formulario>
     </Estrutura>
   );
