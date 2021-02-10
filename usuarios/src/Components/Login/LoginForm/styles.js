b9e3ef339fd0403d23ef8d388bd92cc28f18b32e
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-02-10 18:29:36 -0300

    styles. js do FormLogin
    no formulario: passado margin-top de 30px para distanciar do topo, tentar deixar o max centralizado de acorodo com a Estrutura, padding-bottom 50px distancia um pouco do footer e borda de 8px arredondar. Passado box-shadow e posicionamento da sombra  e cor.
    Input e label: acrescentado cursor pointer.
    
    retirado a estilização do Botao e passado button dentro do formulario e acrescentado button:focus outiline 0.
    
    linky: acrescentado cor red cursor pointer e text-decoration underline, ficar sublinhado.

--- usuarios/src/Components/Login/LoginForm/styles.js
+++ usuarios/src/Components/Login/LoginForm/styles.js
@@ -12,10 +12,13 @@ export const Formulario = styled.form`
   width: 500px;
   height: 580px;
   margin-left: 350px;
-  padding-bottom: 60px;
+  margin-top: 30px;
+  padding-bottom: 50px;
+  border-radius: 8px;
   align-items: center;
   display: grid;
   grid-template-columns: 1fr;
+  box-shadow: 40px 40px 20px black;
   input {
     width: 300px;
     height: 30px;
@@ -23,26 +26,32 @@ export const Formulario = styled.form`
     margin-bottom: 20px;
     margin-left: 80px;
     border-radius: 5px;
+    cursor: pointer;
   }
   label {
     margin-left: 85px;
     padding-top: 60px;
     padding-bottom: 0px;
+    cursor: pointer;
+  }
+  button {
+    width: 150px;
+    height: 30px;
+    border-radius: 5px;
+    margin-left: 150px;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    cursor: pointer;
+  }
+  button:focus {
+    outline: 0;
   }
 `;
 
 export const Titulo = styled.h1`
   margin-left: 100px;
   padding-top: 20px;
-`;
-
-export const Botao = styled.button`
-  width: 150px;
-  height: 30px;
-  border-radius: 5px;
-  margin-left: 150px;
-  margin-top: 20px;
-  margin-bottom: 20px;
+  cursor: pointer;
 `;
 
 export const Linky = styled.section`
@@ -50,4 +59,7 @@ export const Linky = styled.section`
   margin-bottom: 20px;
   margin-left: 50px;
   font-size: 20px;
+  color: Red;
+  cursor: pointer;
+  text-decoration: underline;
 `;
