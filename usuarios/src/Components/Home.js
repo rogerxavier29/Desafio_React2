83a1abf9eaec91697d2cdba0bdd196b04b316114
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-02-10 16:09:17 -0300

    Home.js
    criado Descricao = section como Descrição para poder manipular o input de descrição.
    
    criado Pnome = paragrafo com o nome do usuario.
    
    passado validação nomeProduto . validate() && descrição.validate() - feito para o form. so submeter se ambos os campos tiverem preenchidos.

--- usuarios/src/Components/Home.js
+++ usuarios/src/Components/Home.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import useForm from '../Hooks/useForm';
 import Input from './Form/Input';
 
-import { Estrutura, Title1, Formulario } from './styles';
+import { Estrutura, Title1, Formulario, Descricao, Pnome } from './styles';
 
 const Home = () => {
   const nomeProduto = useForm();
@@ -12,36 +12,38 @@ const Home = () => {
   function handleSubmit(event) {
     event.preventDefault();
 
-    const token = sessionStorage.getItem('token');
+    if (nomeProduto.validate() && descricao.validate()) {
+      const token = sessionStorage.getItem('token');
 
-    const url = 'https://desafionodegx2.herokuapp.com/products';
+      const url = 'https://desafionodegx2.herokuapp.com/products';
 
-    const data = {
-      name: nomeProduto.value,
-      descricao: descricao.value,
-      logo:
-        'sdfsdfsdfssdjasjdijasdjasoijdaosijdaosjdajosajosadjjsdjsaijdojdas.jpg',
-    };
+      const data = {
+        name: nomeProduto.value,
+        descricao: descricao.value,
+        logo:
+          'sdfsdfsdfssdjasjdijasdjasoijdaosijdaosjdajosajosadjjsdjsaijdojdas.jpg',
+      };
 
-    const options = {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    };
+      const options = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
 
-    axios.post(url, data, options).then(
-      (response) => {
-        console.log(response);
-      },
-      (error) => {
-        console.log(error);
-      },
-    );
+      axios.post(url, data, options).then(
+        (response) => {
+          console.log(response);
+        },
+        (error) => {
+          console.log(error);
+        },
+      );
+    }
   }
 
   return (
     <Estrutura>
-      <p>Bem-vindo, {sessionStorage.getItem('name')}</p>
+      <Pnome>Bem-vindo, {sessionStorage.getItem('name')}</Pnome>
       <Title1>Adicionar Produtos</Title1>
       <Formulario onSubmit={handleSubmit}>
         <Input
@@ -51,13 +53,15 @@ const Home = () => {
           placeholder="Nome do Produto"
           {...nomeProduto}
         />
-        <Input
-          label="Descrição"
-          type="text"
-          name="descricao"
-          placeholder="Faça a descrição do Produto"
-          {...descricao}
-        />
+        <Descricao>
+          <Input
+            label="Descrição"
+            type="text"
+            name="descricao"
+            placeholder="Faça a descrição do Produto"
+            {...descricao}
+          />
+        </Descricao>
         <button type="submit">Add Produto</button>
       </Formulario>
     </Estrutura>
