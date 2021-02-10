05be4ec038620499a3484c7dfb8d8f40b0972367
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-02-10 16:05:12 -0300

    styles.js do Home.js
    passado <p> como Pnome, colocado o tamanho da fonte e distanciado o mesmo para esquerda com margin-left 10px.
    
    em Title1, passado margin-left de 385 e margin-top de 30px para o mesmo ficar em cima do form na parte central da Estrutura.
    
    em Formulario: diminuido a margin top, para não ficar muito afastado do title1, passado margin-left de 380px para ficar mais ao centro, repassado outros valores de tamanho e altura, para deixar o mesmo mais quadrado do que retangular como estava antes. Passado um background mais azul pro cinza #b0c4de. Passado form como display grid e não mais flex, ficando os input e button um abaixo do outro.
    Input: passado um margin-left de 18px e margin top de 60px, posicionando os mesmo mais ao centro do form e não muito perto da borda. passado um width e height com valores maiores ajustando o tamanho e altura, passando uma borda bem minuscula, border-radius para deixar mais arrendondado de 5px e o tipo de borda solid.
    label passando um margin-left para ajeitar seu posicionamento.
    
    button: repassado tamanho e altura para ficar mais retangular do que quadrado, posicionado com margin-left, top e bottom para ficar ao centro e abaixo dos inputs e não muito junto ao termino do formulario.
    passado um border-radius de 8px para arrendondar e um cursor pointer.
    passado button:focus outiline 0, para que quando clicar não aparece linha preta retangular.
    criado section como Descrição para poder manipular o input de descrição, que foi passado margin-left para resolver alinhamento como o input nomeProduto

--- usuarios/src/Components/styles.js
+++ usuarios/src/Components/styles.js
@@ -7,31 +7,50 @@ export const Estrutura = styled.div`
   padding-top: 30px;
 `;
 
-export const Title1 = styled.h1`
-  padding-top: 30px;
+export const Pnome = styled.p`
+  font-size: 20px;
   margin-left: 10px;
 `;
 
+export const Title1 = styled.h1`
+  margin-left: 385px;
+  margin-top: 30px;
+`;
+
 export const Formulario = styled.form`
-  margin-top: 10px;
-  margin-left: 15px;
-  width: 1000px;
-  height: 200px;
+  margin-top: 8px;
+  margin-left: 380px;
+  width: 500px;
+  height: 350px;
+  background: #b0c4de;
   border-style: dashed;
-  display: flex;
-  justify-content: space-between;
+  display: grid;
   input {
-    margin-left: 5px;
-    margin-right: 5px;
-    margin-top: 40px;
-    width: 150px;
-    height: 20px;
-    border: 3px;
+    margin-left: 18px;
+    margin-top: 60px;
+    width: 200px;
+    height: 30px;
+    border: 0.1px;
+    border-radius: 5px;
+    border-style: solid;
+  }
+  label {
+    margin-left: 80px;
   }
   button {
-    width: 60px;
-    height: 60px;
-    margin-top: 70px;
-    margin-right: 5px;
+    width: 100px;
+    height: 30px;
+    margin-left: 220px;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    border-radius: 8px;
+    cursor: pointer;
   }
+  button:focus {
+    outline: 0;
+  }
+`;
+
+export const Descricao = styled.section`
+  margin-left: 8px;
 `;
