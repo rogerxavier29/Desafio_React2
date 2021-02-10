0f10b5a18632e1827ed185da0467998ecd0698a5
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-02-10 14:35:55 -0300

    Home.js
    Retirado as constant logo e manual, retirado tambem a function handleLogoChange.
    
    criado 4 constant , token: onde chamo o token de sessionstorage. getItem('token'),
    const url: onde estou passando a api.
    const data: onde passo os dados e seus valores.
    e const options onde passo headers como objeto passand Authorization Bearer ${token}.
    e axios são so os nomes das constant.
    
    em label substituido nome por nomeProd.
    Retirado os input de Imagem e logo, não é solicitado

--- usuarios/src/Components/Home.js
+++ usuarios/src/Components/Home.js
@@ -1,39 +1,42 @@
 import React from 'react';
+import axios from 'axios';
 import useForm from '../Hooks/useForm';
 import Input from './Form/Input';
-import axios from 'axios';
 
 import { Estrutura, Title1, Formulario } from './styles';
 
 const Home = () => {
   const nomeProduto = useForm();
   const descricao = useForm();
-  const [logo, setLogo] = React.useState({});
-  const [manual, setManual] = React.useState({});
 
   function handleSubmit(event) {
     event.preventDefault();
 
-    axios
-      .post('https://desafionodegx2.herokuapp.com/products/', {
-        name: nomeProduto.value,
-        descricao: descricao.value,
-        logo,
-        manual,
-      })
-      .then(
-        (response) => {
-          console.log(response);
-        },
-        (error) => {
-          console.log(error);
-        },
-      );
-  }
-  function handleLogoChange({ target }) {
-    setLogo({
-      raw: target.files[0],
-    });
+    const token = sessionStorage.getItem('token');
+
+    const url = 'https://desafionodegx2.herokuapp.com/products';
+
+    const data = {
+      name: nomeProduto.value,
+      descricao: descricao.value,
+      logo:
+        'sdfsdfsdfssdjasjdijasdjasoijdaosijdaosjdajosajosadjjsdjsaijdojdas.jpg',
+    };
+
+    const options = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    axios.post(url, data, options).then(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      },
+    );
   }
 
   return (
@@ -42,7 +45,7 @@ const Home = () => {
       <Title1>Adicionar Produtos</Title1>
       <Formulario onSubmit={handleSubmit}>
         <Input
-          label="Nome"
+          label="NomeProd"
           type="text"
           name="nomeProduto"
           placeholder="Nome do Produto"
@@ -55,8 +58,6 @@ const Home = () => {
           placeholder="Faça a descrição do Produto"
           {...descricao}
         />
-        <input label="Imagem" type="file" onChange={handleLogoChange} />
-        <input label="Logo" type="file" accept="application/pdf" />
         <button type="submit">Add Produto</button>
       </Formulario>
     </Estrutura>
