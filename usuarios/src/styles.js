9a16a47ec56aa956175fc4ee372466d38be7b52b
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-02-11 16:54:34 -0300

    styles.js do Produto.js
    passado styled-components
    
    importado font Nunito.
    
    passado Estrutura como div: background padrao, altura de 800px para pegar toda estrutura.
    img: altura de 400px e padding-top de 60px se afastando um pouco do topo
    
    passando texto como p:
    margin-left de 580px para ficar ao lado da imagem, margin-top -345px , font-family Nunito cursor pointer, font-size de 20px e e tamanho de 600px não ficar linha perto da margem

---
+++ usuarios/src/styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+<style>
+  @import
+  url('https://fonts.googleapis.com/css2?family=Nunito:wght@200&display=swap');
+</style>;
+
+export const Estrutura = styled.div`
+  background: #87cefa;
+  height: 800px;
+  img {
+    height: 400px;
+    padding-top: 60px;
+  }
+`;
+
+export const Texto = styled.p`
+  margin-left: 580px;
+  margin-top: -345px;
+  font-family: 'Nunito', sans-serif;
+  cursor: pointer;
+  font-size: 20px;
+  width: 600px;
+`;
