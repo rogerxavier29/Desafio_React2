6c7e9dca8384bc5fe30ebc3a57e105e40109298a
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-02-11 12:19:34 -0300

    styles.js do Footer
    passado padding-left de 450px para deixar mais ou centro
    em p passado padding-top: 20px, para deixar mais ao centro

--- usuarios/src/Components/Footer/styles.js
+++ usuarios/src/Components/Footer/styles.js
@@ -4,8 +4,9 @@ export const Estrutura = styled.footer`
   height: 60px;
   background: #4169e1;
   border-top: 3px solid black;
-  align-items: center;
+  padding-left: 450px;
   p {
     color: gold;
+    padding-top: 20px;
   }
 `;
