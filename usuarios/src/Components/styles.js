e200518585166e05df3d52d443b8a0e2166fc428
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-02-11 16:29:39 -0300

    styles.js do Home.js
    importado font Nunito
    passado em Pnome, colocado cor de azul marinho  em span e nunito a font.
    
    Title: alterado margin-left para 510px e margin-top para o mesmo ficar ao centro e não ao lado como estava antes. Passado nunito como font-family e font-size de 25px.
    
    Formulario: passado border-radius de 8px para arrendodar as bordas.
    
    label: font-family Nunito
    
    button: em margin-left aumentado 10px, para ficar ao centro

--- usuarios/src/Components/styles.js
+++ usuarios/src/Components/styles.js
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 
+<style>
+  @import
+  url('https://fonts.googleapis.com/css2?family=Nunito:wght@200&display=swap');
+</style>;
+
 export const Estrutura = styled.div`
   background: #87cefa;
   height: 540px;
@@ -10,25 +15,31 @@ export const Estrutura = styled.div`
 export const Pnome = styled.p`
   font-size: 20px;
   margin-left: 10px;
+  font-family: 'Nunito', sans-serif;
   span {
     font-size: 23px;
     font-weight: bold;
+    font-family: 'Nunito', sans-serif;
+    color: #191970;
     cursor: pointer;
   }
 `;
 
 export const Title1 = styled.h1`
-  margin-left: 385px;
-  margin-top: 30px;
+  margin-left: 510px;
+  margin-top: 35px;
+  font-family: 'Nunito', sans-serif;
+  font-size: 25px;
 `;
 
 export const Formulario = styled.form`
-  margin-top: 8px;
+  margin-top: 30px;
   margin-left: 380px;
   width: 500px;
   height: 350px;
   background: #b0c4de;
   border-style: dashed;
+  border-radius: 8px;
   display: grid;
   box-shadow: 30px 30px 20px black;
   input {
@@ -42,11 +53,12 @@ export const Formulario = styled.form`
   }
   label {
     margin-left: 80px;
+    font-family: 'Nunito', sans-serif;
   }
   button {
     width: 100px;
     height: 30px;
-    margin-left: 220px;
+    margin-left: 230px;
     margin-top: 20px;
     margin-bottom: 20px;
     border-radius: 8px;
