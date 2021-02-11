f1dff0a6f1696b382f89410bcdb9da73548431db
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-02-11 10:33:39 -0300

    styles.js do loginCreate.js
    Importando font Nunito a mesma foi passada em Titulo e label.
    
    Formulario: passado border-radius 8px, deixando mais arredondado, criado um sombreado com box-shadow, na cor black.
    passado cursor pointer em  input , label e Titulo.
    retirado botao e passado button dentro de formulario e button:focus

--- usuarios/src/Components/Login/LoginCreate/styles.js
+++ usuarios/src/Components/Login/LoginCreate/styles.js
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 
+<style>
+  @import
+  url('https://fonts.googleapis.com/css2?family=Nunito:wght@200&display=swap');
+</style>;
+
 export const Estrutura = styled.div`
   background: #87cefa;
   height: 540px;
@@ -16,14 +21,29 @@ export const Formulario = styled.form`
   margin-top: 30px;
   display: grid;
   grid-template-columns: 1fr;
+  border-radius: 8px;
+  box-shadow: 20px 20px 10px black;
   input {
     width: 250px;
     height: 25px;
     margin-left: 120px;
     border-radius: 3px;
+    cursor: pointer;
   }
   label {
     margin-left: 125px;
+    cursor: pointer;
+    font-family: 'Nunito', sans-serif;
+  }
+  button {
+    width: 150px;
+    height: 30px;
+    border-radius: 5px;
+    margin: 0 auto;
+    cursor: pointer;
+  }
+  button:focus {
+    outline: 0;
   }
 `;
 
@@ -32,11 +52,6 @@ export const Titulo = styled.h1`
   height: 0px;
   margin-top: 5px;
   font-size: 30px;
-`;
-
-export const Botao = styled.button`
-  width: 150px;
-  height: 30px;
-  border-radius: 5px;
-  margin: 0 auto;
+  font-family: 'Nunito', sans-serif;
+  cursor: pointer;
 `;
