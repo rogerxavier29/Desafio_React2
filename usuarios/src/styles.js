6b0c17a8525ba2086c061828dcce53b3eda22868
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-02-12 11:04:50 -0300

    styles.js do ProdutoUsado.js
    passado @media (max-width: 500px) com display grid e grid-template columns de 1fr, para ficarem uma abaixo do outro os item.
    
    img passado @media (max-width: 500px), diminuído sua altura para 250px, seu padding-top para 30px e margin-left de 50px.
    
    em texto passado @media (max-width: 500px) passado margin-left 40px, para vir mais pro canto, diminuído font-size para 15px, o tamanho tambem passando par 420px e um padding-top de 220px.

--- usuarios/src/styles.js
+++ usuarios/src/styles.js
@@ -8,9 +8,18 @@ import styled from 'styled-components';
 export const Estrutura = styled.div`
   background: #87cefa;
   height: 800px;
+  @media (max-width: 500px) {
+    display: grid;
+    grid-template-columns: 1fr;
+  }
   img {
     height: 400px;
     padding-top: 60px;
+    @media (max-width: 500px) {
+      height: 250px;
+      padding-top: 30px;
+      margin-left: 50px;
+    }
   }
 `;
 
@@ -21,4 +30,10 @@ export const Texto = styled.p`
   cursor: pointer;
   font-size: 20px;
   width: 600px;
+  @media (max-width: 500px) {
+    margin-left: 40px;
+    font-size: 15px;
+    width: 420px;
+    padding-top: 220px;
+  }
 `;
