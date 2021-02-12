30947dde4b5cd49e4d826a81b14ad0c24a0ffc2b
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-02-11 21:56:11 -0300

    Footer.js
    colocado max-width: 100%, para quando irmos para o responsivo o background fique ainda 100 por cento.
    
    Passando @media(max-width de 500px) para fazer o responsivo part mobile e passando padding-left de 500px para ficar ao centro.

--- usuarios/src/Components/Footer/styles.js
+++ usuarios/src/Components/Footer/styles.js
@@ -3,8 +3,13 @@ import styled from 'styled-components';
 export const Estrutura = styled.footer`
   height: 60px;
   background: #4169e1;
+  max-width: 100%;
   border-top: 3px solid black;
   padding-left: 450px;
+  @media (max-width: 500px) {
+    padding-left: 100px;
+  }
+
   p {
     color: gold;
     padding-top: 20px;
