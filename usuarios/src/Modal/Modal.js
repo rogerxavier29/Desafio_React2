5cef889874267443a4d411c00d0eb6f12f983eea
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-03-10 09:31:46 -0300

    Modal.js
    retirado o import Estrutura.
    importado useState, useEffect,
    import axios.
    importado de styles-component Content e Container.
    passado props  em Modal.
    criado const [prodSelecionado, setProdSelecionado] passado useState vazio.
    criado const token para pegar o mesmo no sessionStorage.
    criado const url e passado a api pegando  productId por props.
    criado const options passado headers e Authorization Bearer ${token}
    passado useEffect,
    e criado const getProducts para pegar os mesmo.
    no html retirado aparte inicial referente a button e Estrutura.
    Criado Content e Container duas <div>, dentro de Container criado button de fechar onde foi passado dentro de onClick props.onClose.
    Logo mais criado uma div e ul e dentro passado um if ternario antes de chamar os produtos selecionado.

--- usuarios/src/Modal/Modal.js
+++ usuarios/src/Modal/Modal.js
@@ -1,17 +1,61 @@
-import React, { useState } from 'react';
-import Estrutura from './Estrutura/Estrutura';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const Modal = () => {
-  const [isModalVisible, setIsModalVisible] = useState(false);
+import { Content, Container } from './styles';
+
+const Modal = (props) => {
+  const [prodSelecionado, setProdSelecionado] = useState();
+
+  const token = sessionStorage.getItem('token');
+
+  const url = `https://desafionodegx2.herokuapp.com/products/${props.productId}`;
+
+  const options = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  useEffect(() => {
+    console.log('aqui');
+    getProducts();
+  }, []);
+  const getProducts = async () => {
+    axios
+      .get(url, options)
+      .then((res) => {
+        console.log(res.data);
+        setProdSelecionado(res.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
   return (
     <div>
-      <button onClick={() => setIsModalVisible(true)}>Open</button>
-      {isModalVisible ? (
-        <Estrutura onClose={() => setIsModalVisible(false)}>
-          <h1>Modal</h1>
-        </Estrutura>
-      ) : null}
+      <Content id="modal">
+        <Container>
+          <button onClick={props.onClose}>X</button>
+          <div>
+            <ul>
+              {prodSelecionado && (
+                <li>
+                  <div>
+                    <p key={prodSelecionado[0].id}>
+                      Name: {prodSelecionado[0].name}
+                    </p>
+                    <p>Descrição: {prodSelecionado[0].descricao} </p>
+
+                    <p>Logo:{prodSelecionado[0].logo}</p>
+                    <p>Manual:{prodSelecionado[0].manual}</p>
+                  </div>
+                </li>
+              )}
+            </ul>
+          </div>
+        </Container>
+      </Content>
     </div>
   );
 };
