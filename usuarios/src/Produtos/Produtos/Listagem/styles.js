399508920b28afb0140196cbee542bf3e8e2297d
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-03-10 16:24:28 -0300

    styles.js do ListaProduto.js
    Estilizando o botao de Produtos
    posicionado o mesmo margin-top, bottom e left ambos com 10px.
    
    1° div como Geral, passado um background verde suave e width 100%.
    
    2° div como ListaProd, passado display grid , z-index 100px, margin-top 5px, passado tambem border-style groove e bottom de 0.5rem.
    <p> onde fica os dados do produto, feito o posicionamento margin-top 7px, margin-bottom de 15px e margin-top de 7px, margin-left: 5px.
    
    button de Detalhes, width 100px, margin-top: 5px; margin-left: 400px

---
+++ usuarios/src/Produtos/Produtos/Listagem/styles.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const Botao = styled.button`
+  margin-top: 10px;
+  margin-bottom: 10px;
+  margin-left: 10px;
+`;
+
+export const Geral = styled.div`
+  background: #f0fff0;
+  width: 100%;
+`;
+
+export const ListaProd = styled.div`
+  display: grid;
+  z-index: 100px;
+  margin-top: 5px;
+  border-style: groove;
+  border-bottom: 0.5rem;
+
+  p {
+    margin-top: 7px;
+    margin-bottom: 15px;
+    margin-left: 5px;
+  }
+  button {
+    width: 100px;
+    margin-top: 5px;
+    margin-left: 400px;
+  }
+`;
