be754c379ca12fca148774421b73fb60feee3d03
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-03-11 16:35:49 -0300

    styles.js de Modal.js
    em Content: height passa pra 90vh, position passa para fixed, pois antes o modal não acompanhava o scroll da tela fica parado no topo .
    
    Container: width de 360px para 400px

--- usuarios/src/Modal/styles.js
+++ usuarios/src/Modal/styles.js
@@ -2,8 +2,8 @@ import styled from 'styled-components';
 
 export const Content = styled.div`
   width: 100%;
-  height: 100vh;
-  position: absolute;
+  height: 90vh;
+  position: fixed;
   z-index: 100px;
   background: rgba(0, 0, 0, 0.8);
   display: flex;
@@ -13,10 +13,11 @@ export const Content = styled.div`
 
 export const Container = styled.div`
   background: #fff;
-  width: 360px;
+  width: 400px;
   height: 250px;
   margin-top: -60px;
   border-radius: 3px;
+
   p {
     margin-top: 20px;
     margin-bottom: 10px;
