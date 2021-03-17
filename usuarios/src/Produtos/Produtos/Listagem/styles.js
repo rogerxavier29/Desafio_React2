63da1d81d389690b69ae3dc296ee118bb137a162
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-03-17 15:44:10 -0300

    ListaProduto.js
    no if da função acrescentado o produto
    
    acrescentado if que chama a janela do alert no alterarProd

--- usuarios/src/Produtos/Produtos/Listagem/styles.js
+++ usuarios/src/Produtos/Produtos/Listagem/styles.js
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 
+<style>
+  @import
+  url('https://fonts.googleapis.com/css2?family=Nunito:wght@200&display=swap');
+</style>;
+
 export const Geral = styled.div`
   background: #f0fff0;
 `;
@@ -10,10 +15,15 @@ export const ListaProd = styled.div`
   margin-top: 5px;
   border-style: groove;
   border-top: 0.5rem;
+
   p {
     margin-top: 7px;
     margin-bottom: 15px;
     margin-left: 5px;
+    font-family: 'Nunito', sans-serif;
+    @media (max-width: 500px) {
+      font-size: 13px;
+    }
   }
 `;
 
@@ -21,6 +31,10 @@ export const Botoes = styled.div`
   display: flex;
   justify-content: center;
   margin-left: 170px;
+  @media (max-width: 500px) {
+    margin-left: 150px;
+    margin-top: -6px;
+  }
 `;
 
 export const BtnProd = styled.button`
@@ -31,6 +45,11 @@ export const BtnProd = styled.button`
   border-radius: 3px;
   background: #00ff7f;
   cursor: pointer;
+  @media (max-width: 500px) {
+    width: 67px;
+    height: 23px;
+    margin-left: 40px;
+  }
 `;
 
 export const Btndel = styled.button`
@@ -42,6 +61,11 @@ export const Btndel = styled.button`
   background: #b22222;
   color: #ffff;
   cursor: pointer;
+  @media (max-width: 500px) {
+    width: 65px;
+    height: 23px;
+    margin-left: 20px;
+  }
 `;
 
 export const BtnEdit = styled.button`
@@ -52,6 +76,11 @@ export const BtnEdit = styled.button`
   border-radius: 3px;
   background: #ffd700;
   cursor: pointer;
+  @media (max-width: 500px) {
+    width: 65px;
+    height: 23px;
+    margin-left: 20px;
+  }
 `;
 
 export const SectionInputs = styled.section`
@@ -88,6 +117,7 @@ export const ListaProd2 = styled.div`
     margin-top: 7px;
     margin-bottom: 15px;
     margin-left: 5px;
+    font-family: 'Nunito', sans-serif;
   }
 
   button {
